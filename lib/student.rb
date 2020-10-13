a337b16522007e3469d04646f0af9412c95d39ac
Christopher Dutcher <cadutcher@icloud.com>
2020-10-13 17:37:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,5 +31,6 @@ class Student
     INSERT INTO students (name, grade) VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, @name, @grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
 end
